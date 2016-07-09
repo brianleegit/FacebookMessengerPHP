b7d4824b097260598aa3ff9c0fca155e1be3e1be
brianlee <leebrian699@gmail.com>
2016-07-09 16:39:13 +0800

    sdsdsdsdr21122

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
-if(isset($_REQUEST['hub_challenge'])) {
-    $challenge = $_REQUEST['hub_challenge'];
-    $hub_verify_token = $_REQUEST['hub_verify_token'];
-}
-if ($hub_verify_token === 'brian') {
-  echo $challenge;
-}
+// if(isset($_REQUEST['hub_challenge'])) {
+//     $challenge = $_REQUEST['hub_challenge'];
+//     $hub_verify_token = $_REQUEST['hub_verify_token'];
+// }
+// if ($hub_verify_token === 'brian') {
+//   echo $challenge;
+// }
+echo "fuck";
+?>

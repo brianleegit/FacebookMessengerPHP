8c5d435e6b0e731fe8526a4b33837713227eeb92
brianlee <leebrian699@gmail.com>
2016-07-09 15:23:10 +0800

    trert

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-$access_token = "EAAZAsaRTFGuQBAHkWDMAHNZAoGUyHFnRGEinLFT08SZAtqkDGUNeZBMLC0BZCj4mS9w4pgQaFfTHKZCHqMG8fCWcyFdTB3nEfdKLGp8WZB2CDHZCSsWWAZBiJp2WPuTbfJwsLql6xAqLEN0EHwuyCzvNxLTM7gC0Jyd5rOraLyCJrQQZDZD";
-$verify_token = "brian";
-$hub_verify_token = null;
- 
-if(isset($_REQUEST['hub_challenge'])) {
-    $challenge = $_REQUEST['hub_challenge'];
-    $hub_verify_token = $_REQUEST['hub_verify_token'];
-}
- 
- 
-if ($hub_verify_token === $verify_token) {
-    echo $challenge;
-}
+$verify_token = "brian"; 
+// Verify token 
+if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) { 
+	echo $_REQUEST['hub_challenge']; 
+} 

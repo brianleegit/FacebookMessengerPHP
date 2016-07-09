482101affc27c5c48d19da0a0e297c32ffa98f1a
brianlee <leebrian699@gmail.com>
2016-07-09 15:40:54 +0800

    sdsdsdsdr2

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php
+<?php 
 $verify_token = "brian"; 
 // Verify token 
 
 if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) { 
 	echo $_REQUEST['hub_challenge']; 
-}
-?> 
+} 
+?>

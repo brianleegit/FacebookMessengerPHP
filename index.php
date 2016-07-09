9a4888c36deb42d7b9d5b72da43cfadc648d6600
brianlee <leebrian699@gmail.com>
2016-07-09 16:32:42 +0800

    sdsdsdsqwdr2

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-$challenge = $_REQUEST['hub_challenge'];
-$verify_token = $_REQUEST['hub_verify_token'];
 
-if ($verify_token === 'brian') {
+if(isset($_REQUEST['hub_challenge'])) {
+    $challenge = $_REQUEST['hub_challenge'];
+    $hub_verify_token = $_REQUEST['hub_verify_token'];
+}
+if ($hub_verify_token === 'brian') {
   echo $challenge;
 }

650149e6f241183724c565cec39726cc200c4913
brianlee <leebrian699@gmail.com>
2016-07-11 22:28:01 +0800

    updatse2

--- src/routes.php
+++ src/routes.php
@@ -27,9 +27,10 @@ $app->get('/createlog', function ($request, $response, $args) {
 
 $app->get('/webhook', function ($request, $response, $args) {
     $verify_token = "brian";
-	$request = $request->getQueryParams();  
- 
-    if(!empty($request['hub_verify_token']) && !empty($request['hub_mode']) && $request['hub_mode'] == 'subscribe' && $request['hub_verify_token'] == $verify_token){
+	$request = $request->getQueryParams(); 
+
+    if(!empty($request['hub_verify_token']) && !empty($request['hub_mode']) 
+        && $request['hub_mode'] == 'subscribe' && $request['hub_verify_token'] == $verify_token){
       	return $response->write($request['hub_challenge'])->withStatus(200);
     }else {
       	return $response->withStatus(403);	

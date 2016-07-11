1995b46f2ce5e9852ae3ab913517e814e77488da
brianlee <leebrian699@gmail.com>
2016-07-11 22:54:11 +0800

    update2

--- src/routes.php
+++ src/routes.php
@@ -48,7 +48,7 @@ $app->get('/webhook', function ($request, $response, $args) {
 });
 
 $app->post('/webhook', function ($request, $response, $args) {
-    $body = json_encode(file_get_contents('php://input'));   
+    $body = json_decode(file_get_contents('php://input'));   
     $this->db->table('facebook_logs')->insert([
         ['requests' => $body]       
     ]);

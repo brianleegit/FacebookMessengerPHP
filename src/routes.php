9740b96f88a3087b9cbc89ef57801d88ebb9a156
brianlee <leebrian699@gmail.com>
2016-07-11 12:16:36 +0800

    updatse2

--- src/routes.php
+++ src/routes.php
@@ -4,19 +4,19 @@ $app->get('/test', function ($request, $response, $args) {
   return $response->write("fuck");
 });
 
-$app->get('/webhook', function ($request, $response, $args) {
-	$verify_token = "brian";
-	$request = $request->getQueryParams();  
+// $app->get('/webhook', function ($request, $response, $args) {
+// 	$verify_token = "brian";
+// 	$request = $request->getQueryParams();  
  
-  if(!empty($request['hub_verify_token']) && !empty($request['hub_mode']) && $request['hub_mode'] == 'subscribe' && $request['hub_verify_token'] == $verify_token){
-  	return $response->write($request['hub_challenge'])->withStatus(200);
-  }else {
-  	return $response->withStatus(403);	
-  }
-});
+//   if(!empty($request['hub_verify_token']) && !empty($request['hub_mode']) && $request['hub_mode'] == 'subscribe' && $request['hub_verify_token'] == $verify_token){
+//   	return $response->write($request['hub_challenge'])->withStatus(200);
+//   }else {
+//   	return $response->withStatus(403);	
+//   }
+// });
 
-$app->post('/webhook', function ($request, $response, $args) {
-   $body = $request->getParsedBody();
+// $app->post('/webhook', function ($request, $response, $args) {
+//    $body = $request->getParsedBody();
    
 
-});
+// });
